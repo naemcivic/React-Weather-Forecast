96a6e463a6cedb1a9cd19734207951c89cc3cddb
naemcivic <naemcivic@gmail.com>
2016-04-08 12:39:48 -0400

    add IndexRoute to routes, create HomeContainer to render Home component and add a background picture to app/images

---
+++ app/components/Home.js
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var styles = {
+  container: {
+  backgroundSize: 'cover',
+  backgroundImage: "url('app/images/pattern.svg')",
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%'
+  },
+  header: {
+  fontSize: 45,
+  color: '#fff',
+  fontWeight: 100,
+  },
+}
+
+function Home (props) {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.header}>Enter a City or Country</h1>
+    </div>
+  )
+}
+
+module.exports = Home;
