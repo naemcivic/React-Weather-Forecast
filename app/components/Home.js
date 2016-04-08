c20e4c8dd290ddb0cbf388ad3346b253dcb05f27
naemcivic <naemcivic@gmail.com>
2016-04-08 14:46:27 -0400

    create GetCityContainer to define state and pass down callback to child component GetCity. create GetCity component to render views for input and button to collect data and update state

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var GetCityContainer = require('../containers/GetCityContainer');
 
 var styles = {
   container: {
@@ -21,7 +22,8 @@ var styles = {
 function Home (props) {
   return (
     <div style={styles.container}>
-      <h1 style={styles.header}>Enter a City or Country</h1>
+      <h1 style={styles.header}>Enter a Country or City</h1>
+      <GetCityContainer />
     </div>
   )
 }
